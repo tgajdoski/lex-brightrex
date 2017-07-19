01e7d12a8b17d72a06bebf98607145786c3b6a2d
Toni Gajdoski <t.gajdoski@gmail.com>
2017-07-19 15:15:27 +0200

    play with mysql query

--- handler.js
+++ handler.js
@@ -3,6 +3,21 @@
 const dispatch = require('./dispatch');
 const userFavorites = require('./userEmail/userEmail');
 
+
+
+// test mysql
+var mysql = require('mysql');
+var pool = mysql.createPool({
+      host     : 'app.cfx8l12fysob.us-west-2.rds.amazonaws.com',
+      user     : 'ffi',
+      password : 'Firm2825',
+      database : 'app'
+});
+
+
+
+
+
 module.exports.intents = (event, context, callback) => {
   try {
     console.log(`event.bot.name=${event.bot.name}`);
@@ -39,6 +54,44 @@ module.exports.saveUserEmail = (event, context, callback) => {
 
 
    // callback(null, null);
+};
+
+
+
+
+
+
+module.exports.query = (event, context, callback) => {
+   
+try {
+  pool.getConnection(function(err, connection) {
+
+     var ret = [];
+
+  // Use the connection
+   var qqq = "select id, date, lot from job where market_id = 5 limit 10";
+  console.log(qqq);
+  
+  connection.query(qqq, function (error, results, fields) {
+  
+     for (var i of results) {
+        console.log(i);
+        console.log(i.id );
+        ret.push(i.id + ";" + i.date + ";" + i.lot);
+     }
+    context.succeed(JSON.stringify(ret)); 
+       console.log('AAA' +ret);
+    connection.release();
+
+    // Handle error after the release.
+      if (error) throw error;
+    // Don't use the connection here, it has been returned to the pool.
+    });
+  });
+    } catch (error) {
+        context.fail(`Exceptiodn: ${error}`)
+       //  callback(error, 'sdasdasdasdasd');
+    }
 };
 
 
---
+++ mysqlManager.js
@@ -0,0 +1,53 @@
+'use strict';
+
+//exports.handler = (event, context, callback) => {
+
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+      host     : 'app.cfx8l12fysob.us-west-2.rds.amazonaws.com',
+      user     : 'ffi',
+      password : 'Firm2825',
+      database : 'app'
+});
+
+
+module.exports.getQuery = () => {
+ 
+try {
+  pool.getConnection(function(err, connection) {
+  // Use the connection
+   var qqq = "select id, date, lot from job where market_id = 5 limit 10";
+  console.log(qqq);
+  connection.query(qqq, function (error, results, fields) {
+    // And done with the connection.
+
+  //  var message = JSON.parse(JSON.stringify(results[0].brojki));
+  //         var messagefinal = "Total job number for Market: " + Market + " is " + message;
+  //         var response = `{
+  //           "dialogAction": {
+  //               "type": "Close",
+  //               "fulfillmentState": "Fulfilled",
+  //               "message": {
+  //                   "contentType": "PlainText",
+  //                   "content":  "${messagefinal}"
+  //                   }
+  //               }
+  //           }`;
+
+            
+    context.succeed( JSON.parse(results));
+                                      
+    connection.release();
+
+    // Handle error after the release.
+      if (error) throw error;
+    // Don't use the connection here, it has been returned to the pool.
+    });
+  });
+  } catch (error) {
+    context.fail(`Exception: ${error}`)
+  //callback(error, 'sdasdasdasdasd');
+  }
+};
+
