c50eae060bc476c98cbe16c15b240ce37052960c
Toni Gajdoski <t.gajdoski@gmail.com>
2017-07-20 15:11:01 +0200

    response card lambda nodejs

--- handler.js
+++ handler.js
@@ -42,7 +42,6 @@ module.exports.saveUserEmail = (event, context, callback) => {
     //     callback(err);
     //   }
     userFavorites(userId, email);
-
      try {
           console.log(`event.bot.name=${event.bot.name}`);
           dispatch(event).then(response => {
@@ -51,47 +50,33 @@ module.exports.saveUserEmail = (event, context, callback) => {
         } catch (err) {
           callback(err);
         }
-
-
    // callback(null, null);
 };
 
 
-
-
-
-
 module.exports.query = (event, context, callback) => {
-   
-try {
-  pool.getConnection(function(err, connection) {
-
-     var ret = [];
-
-  // Use the connection
-   var qqq = 'select id, DATE_FORMAT(date, "%M %d %Y") as datum, lot from job where market_id = 5 limit 10';
-  console.log(qqq);
-  
-  connection.query(qqq, function (error, results, fields) {
-  
-     for (var i of results) {
-        console.log(i);
-        console.log(i.id );
-        ret.push(i.id + ";" +  i.datum + ";" + i.lot);
-     }
-    context.succeed(JSON.stringify(ret)); 
-       console.log('AAA' +ret);
-    connection.release();
-
-    // Handle error after the release.
-      if (error) throw error;
-    // Don't use the connection here, it has been returned to the pool.
-    });
-  });
+    try {
+      pool.getConnection(function(err, connection) {
+        var ret = [];
+      // Use the connection
+      var qqq = 'select id, DATE_FORMAT(date, "%M %d %Y") as datum, lot from job where market_id = 5 limit 10';
+      console.log(qqq);
+      connection.query(qqq, function (error, results, fields) {
+        for (var i of results) {
+            console.log(i);
+            console.log(i.id );
+            ret.push(i.id + ";" +  i.datum + ";" + i.lot);
+        }
+        context.succeed(JSON.stringify(ret)); 
+          console.log('AAA' +ret);
+        connection.release();
+        // Handle error after the release.
+          if (error) throw error;
+        // Don't use the connection here, it has been returned to the pool.
+        });
+      });
     } catch (error) {
         context.fail(`Exceptiodn: ${error}`)
        //  callback(error, 'sdasdasdasdasd');
     }
 };
-
-
